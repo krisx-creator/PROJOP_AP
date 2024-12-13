3e1f9b7281fdcc07cf33e26b622b061f40290170
krisx-creator <krystianslawinski1708@gmail.com>
2024-12-13 20:20:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,110 @@
-//pytanie pierwsze
-var fsa = 0;
-var dfsd = prompt("Jak nazywał się najwazniejszy bog grecki?");
-var sasa = prompt("Jak nazywał sie przywodca grecki ktory walczyl w bitwie pod Termopilami");
-var fds = parseInt(prompt("W którym roku p.n.e odbyła się bitwa pod Termopilami?"));
+const pytania = [
+  {
+      pytanie: "W którym roku była bitwa pod termopilami?",
+      odp: [
+        { text: "470 p.n.e.", correct: false},
+        { text: "480 p.n.e.", correct: true},
+        { text: "477 p.n.e.", correct: false},
+        { text: "486 p.n.e.", correct: false},
+      ]
+  },
+  {
+    pytanie: "Jak nazywał się dowódca który dowodził wojskami po stronie Grecji w bitwie pod termopilami?",
+    odp: [
+      { text: "Leonidas", correct: true},
+      { text: "Kserkses", correct: false},
+      { text: "Temistokles", correct: false},
+      { text: "Perykles", correct: false},
+    ]
 
-//instrukcja sprawdzajaca odpowiedz
-switch(dfsd) {
-    //poprawne odpowiedzi
-    case "zeus":
-    case "Zeus":
-    case "ZEUS":
-        fsa++;
-        alert("poprawna odpowiedz, masz: "+fsa +" punktów");
-    break;
+  },
+  {
+  pytanie: "Najważniejszy bóg Grecki",
+  odp: [
+    { text: "Posejdon", correct: false},
+    { text: "Ares", correct: false},
+    { text: "Afrodyta", correct: false},
+    { text: "Zeus", correct: true},
+  ]
+  }
+];
 
-    default:
-        ("zła odpowiedź, masz: "+fsa+" punktów");
+const pytanieEl = document.getElementById("pytanie");
+const przyciskOdp = document.getElementById("wybierzodp");
+const przyciskDalej = document.getElementById("dalej");
+
+let currentquestionindex = 0;
+
+function startQuiz() {
+  currentquestionindex = 0;
+  przyciskDalej.innerHTML = "Dalej";
+  showQuestion();
 }
 
-//pytanie drugie
-switch(sasa) {
-        case "Leonidas":
-        case "leonidas":
-        case "Leonidas":
-        case "Leonidas I":
-            fsa++;
-            document.write("poprawna odpowiedź, masz: "+fsa+" punktów");
-        break;
-        default:
-            document.write("zła odpowiedź, masz: "+fsa+ " punktów");
+function showQuestion() {
+  reset();
+  let currentQuestion = pytania[currentquestionindex];
+  let nrPytania = currentquestionindex + 1;
+  pytanieEl.innerHTML = nrPytania + ". " + currentQuestion.pytanie;
+
+  currentQuestion.odp.forEach(odp => {
+    const button = document.createElement("button");
+    button.innerHTML = odp.text;
+    button.classList.add("btn");
+    wybierzodp.appendChild(button);
+    if(odp.correct) {
+      button.dataset.correct = odp.correct;
     }
-
-
-if(fds == 480) {
-    fsa++;
-        document.write("poprawna odpowiedz, masz: "+fsa +" punktów");
+    
+    button.addEventListener("click", slelectanswer);
+    
+  });
 }
 
-else {
-    document.write("zła odpowiedź, masz: "+fsa+ " punktów");
+function slelectanswer(e) {
+  const selectedbtn = e.target;
+  const czyPoprawna = selectedbtn.dataset.correct === "true";
+  if(czyPoprawna) {
+    selectedbtn.classList.add("correct");
+  }
+  else {
+    selectedbtn.classList.add("incorrect");
+  }
+  Array.from(wybierzodp.children).forEach(button => {
+    if(button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  przyciskDalej.style.display = "block";
 }
+
+  function reset() {
+    przyciskDalej.style.display="none";
+    while(wybierzodp.firstChild) {
+      wybierzodp.removeChild(wybierzodp.firstChild);
+    }
+  }
+
+  function nastepnyprzycisk() {
+    currentquestionindex++;
+    if(currentquestionindex < pytania.length) {
+      showQuestion();
+    }
+    
+  }
+
+  przyciskDalej.addEventListener("click", ()=>{  
+    if(currentquestionindex < pytania.length) {
+      nastepnyprzycisk();
+    } 
+    else {
+      startQuiz();
+    }
+  });
+  
+startQuiz();
+
 
 
 
